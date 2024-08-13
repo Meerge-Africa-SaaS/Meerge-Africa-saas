8c4755c51ba18bf0024b84209a2ad162a63f731c
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-08-13 11:00:03 +0100

    chore: Remove core prefix from urls

--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,22 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
-from . import api
-from . import views
-from . import htmx
-
+from . import api, htmx, views
 
 router = routers.DefaultRouter()
 router.register("User", api.UserViewSet)
 
 urlpatterns = (
     path("api/v1/", include(router.urls)),
-    path("core/User/", views.UserListView.as_view(), name="core_User_list"),
-    path("core/User/create/", views.UserCreateView.as_view(), name="core_User_create"),
-    path("core/User/detail/<str:pk>/", views.UserDetailView.as_view(), name="core_User_detail"),
-    path("core/User/update/<str:pk>/", views.UserUpdateView.as_view(), name="core_User_update"),
-    path("core/User/delete/<str:pk>/", views.UserDeleteView.as_view(), name="core_User_delete"),
+    path("User/", views.UserListView.as_view(), name="core_User_list"),
+    path("User/create/", views.UserCreateView.as_view(), name="core_User_create"),
+    path("User/detail/<str:pk>/", views.UserDetailView.as_view(), name="core_User_detail"),
+    path("User/update/<str:pk>/", views.UserUpdateView.as_view(), name="core_User_update"),
+    path("User/delete/<str:pk>/", views.UserDeleteView.as_view(), name="core_User_delete"),
 
-    path("core/htmx/User/", htmx.HTMXUserListView.as_view(), name="core_User_htmx_list"),
-    path("core/htmx/User/create/", htmx.HTMXUserCreateView.as_view(), name="core_User_htmx_create"),
-    path("core/htmx/User/delete/<str:pk>/", htmx.HTMXUserDeleteView.as_view(), name="core_User_htmx_delete"),
+    path("core_User_signin"),
+
+    path("htmx/User/", htmx.HTMXUserListView.as_view(), name="core_User_htmx_list"),
+    path("htmx/User/create/", htmx.HTMXUserCreateView.as_view(), name="core_User_htmx_create"),
+    path("htmx/User/delete/<str:pk>/", htmx.HTMXUserDeleteView.as_view(), name="core_User_htmx_delete"),
 )
