44d7889dea00759c51e2b644fb8f3647da79182e
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-08-13 11:31:49 +0100

    refactor: Update user signin view and form
    This commit refactors the user signin view and form in the `core/views.py` and `core/forms.py` files.
    
    In `core/views.py`, the `UserSigninView` class is added to handle user authentication. It sets the `next_page` attribute to "/" and disables redirection for authenticated users. The template used for rendering the signin page is set to "core/user_signin.html".
    
    In `core/forms.py`, the `UserSigninForm` class is added to customize the authentication form. It removes the username field and replaces it with an email field. The form also includes logic to authenticate the user using the email and password.
    
    These changes improve the user signin functionality and enhance the security of the authentication process.

--- core/urls.py
+++ core/urls.py
@@ -13,8 +13,7 @@ urlpatterns = (
     path("User/detail/<str:pk>/", views.UserDetailView.as_view(), name="core_User_detail"),
     path("User/update/<str:pk>/", views.UserUpdateView.as_view(), name="core_User_update"),
     path("User/delete/<str:pk>/", views.UserDeleteView.as_view(), name="core_User_delete"),
-
-    path("core_User_signin"),
+    path("User/signin/", views.UserSigninView.as_view(), name="core_User_signin"),
 
     path("htmx/User/", htmx.HTMXUserListView.as_view(), name="core_User_htmx_list"),
     path("htmx/User/create/", htmx.HTMXUserCreateView.as_view(), name="core_User_htmx_create"),
