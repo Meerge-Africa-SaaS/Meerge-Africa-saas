c6b38412f2f7f7d926d9ac0bb31d186ebf19d3ef
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2024-08-24 11:51:52 +0100

    fix2

--- core/CustomFiles/CustomBackend.py
+++ core/CustomFiles/CustomBackend.py
@@ -10,3 +10,9 @@ class PhoneAuthBackend(ModelBackend):
                 return user
         except UserModel.DoesNotExist:
             return None
+        
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
