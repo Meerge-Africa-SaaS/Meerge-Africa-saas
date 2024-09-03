9111058f2a584dc9d927e60aa6bba07bed7deba9
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-09-03 15:09:27 +0100

    chore: Load secrets from environment

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for PLSOM project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+from config.env import env
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{env}")
 application = get_wsgi_application()
