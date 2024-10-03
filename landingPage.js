849391534d606b0ce9c7b651e858e6953c7115a4
GloryKachi <kachikachina@gmail.com>
2024-10-03 15:51:33 +0100

    second push for merge

--- landingPage.js
+++ landingPage.js
@@ -8,17 +8,20 @@ menuBtn.addEventListener("click", () => {
 window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
   const links = document.querySelectorAll("nav a");
+  const button = document.querySelector("nav button");
   if (window.scrollY > 50) {
     nav.classList.add("bg-white", "shadow-lg");
     links.forEach((link) => {
       link.classList.remove("text-white");
       link.classList.add("text-black");
     });
+    button.style.backgroundColor = "#0E2254";
   } else {
     nav.classList.remove("bg-white", "shadow-lg");
     links.forEach((link) => {
       link.classList.remove("text-black");
       link.classList.add("text-white");
     });
+    button.style.backgroundColor = "#ff4101";
   }
 });
