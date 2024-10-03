04b7dbe3286b5913a5fca3cd57d75d46fe1001dd
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-10-03 16:54:54 +0100

    feat: Add business info screens
    This commit adds the business profile screens along with modals to edit the details of the business.

---
+++ restaurants/static/js/update-business-info-modal.js
@@ -0,0 +1,21 @@
+(() => {
+    /**@type {HTMLDivElement} */
+    const modal = document.getElementById('update-business-info-modal');
+    /**@type {HTMLButtonElement} */
+    const cancelBtn = modal.querySelector('button.cancel');
+    /**@type {HTMLButtonElement} */
+    const addBtn = modal.querySelector('button.add');
+
+    /**@type {HTMLDivElement} */
+    const emailInput = document.getElementById("email-input")
+    /**@type {HTMLDivElement} */
+    const roleInput = document.getElementById("role-input")
+
+    cancelBtn?.addEventListener('click', () => {
+        modal.classList.remove('active')
+    })
+
+    addBtn?.addEventListener('click', () => {
+        modal.classList.remove('active')
+    });
+})();
