fc481a32efd94ccf3296cac985943e675070502d
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-10-26 21:13:59 +0100

    Revert "refactor: Update Plunk API key in mail settings"
    This reverts commit 30548e8ef9f8a7f71bd39c1674bc100efe72c0c4.

--- config/settings/mail.py
+++ config/settings/mail.py
@@ -1,6 +1,6 @@
 
 PLUNK_API_KEY = getenv(
-    "PLUNK_API_KEY", "s" + "k" + "_cdda06aef9d42118cc1e0a9726171311fe2314c2df3cbf81"
+    "PLUNK_API_KEY", "s" + "k" + "cdda06aef9d42118cc1e0a9726171311fe2314c2df3cbf81"
 )
 PLUNK_API_URL = getenv("PLUNK_API_URL", "https://api.useplunk.com/v1/send")
 
