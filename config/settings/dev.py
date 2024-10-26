519f0a45c7cbe04e5e89bcfaf57e611ace57c8e3
omniotosho <toloruntotosho@gmail.com>
2024-10-27 00:43:57 +0100

    re re sre

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -18,16 +18,19 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-from .geo import *
-from .auth import *
-from .mail import *
-from .api import *
-from .banking import *
-from .restaurants import *
-from .customers import *
-from .inventory import *
-from .orders import *
-from .djext import *
-from .reload import *
-from .compressor import *
+try:
+    from .sre import *
+    from .geo import *
+    from .auth import *
+    from .mail import *
+    from .api import *
+    from .banking import *
+    from .restaurants import *
+    from .customers import *
+    from .inventory import *
+    from .orders import *
+    from .djext import *
+    from .reload import *
+    from .compressor import *
+except ImportError:
+    pass
