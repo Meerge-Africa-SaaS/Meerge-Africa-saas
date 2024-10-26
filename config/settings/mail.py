57791e0deeeebda4144ea2c284598aa2cdca8c5c
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-10-26 21:20:36 +0100

    chore: Remove plunk API key

--- config/settings/mail.py
+++ config/settings/mail.py
@@ -1,8 +1,6 @@
 
-PLUNK_API_KEY = getenv(
-    "PLUNK_API_KEY", "s" + "k" + "cdda06aef9d42118cc1e0a9726171311fe2314c2df3cbf81"
-)
-PLUNK_API_URL = getenv("PLUNK_API_URL", "https://api.useplunk.com/v1/send")
+from config.environ import getboolenv
+
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
