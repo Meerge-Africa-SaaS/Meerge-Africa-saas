4bb5ce8170089f0e188f8968e574d5468914b74e
omniotosho <toloruntotosho@gmail.com>
2024-10-27 01:44:11 +0100

    settings re re

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -32,5 +32,6 @@ try:
     from .djext import *
     from .reload import *
     from .compressor import *
-except ImportError:
-    pass
+except Exception as e:
+    print(f"error loading. .. {__file__}")
+    print(e)
