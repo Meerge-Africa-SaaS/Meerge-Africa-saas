110eb173e27805f6d8d22e22c83cf5443187abe3
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-10-27 19:21:07 +0100

    chore: Configured production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,9 +1,25 @@
-# ruff: noqa: F403,F405
+# ruff: noqa: F403,F405,E402
+from config.env import getlistenv, getenv
 from .base import *
 
 DEBUG = False
+ALLOWED_HOSTS =  getlistenv("DJANGO_ALLOWED_HOSTS", [])
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 try:
     from .local import *  # type: ignore
 except ImportError:
     pass
+
+from .geo import *
+from .auth import *
+from .mail import *
+from .api import *
+from .banking import *
+from .restaurants import *
+from .customers import *
+from .inventory import *
+from .orders import *
+from .djext import *
+from .reload import *
+from .compressor import *
