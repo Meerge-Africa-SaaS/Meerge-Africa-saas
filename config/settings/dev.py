941752974c126697d7a280c95cb2c65ad2799ec7
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-10-27 19:23:34 +0100

    chore: Remove databases settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,5 +1,5 @@
 # ruff: noqa: F405,F403,E402
-from config.environ import getenv
+from config.env import getenv
 #from mail import EMAIL_BACKEND, EMAIL_PORT, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_USE_TLS
 from .base import *  # noqa: F403
 
@@ -11,13 +11,6 @@ SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
 from .geo import *
 from .auth import *
