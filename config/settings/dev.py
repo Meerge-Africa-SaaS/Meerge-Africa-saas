d2a01726b2dc488792228aa5f71b804e0c99d97e
omniotosho <toloruntotosho@gmail.com>
2024-10-28 08:07:35 +0100

    settings re re

---
+++ config/settings/dev.py
@@ -0,0 +1,32 @@
+# ruff: noqa: F405,F403,E402
+from config.environ import getenv
+#from mail import EMAIL_BACKEND, EMAIL_PORT, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_USE_TLS
+from .base import *  # noqa: F403
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+try:
+    from .sre import *
+    from .geo import *
+    from .banking import *
+    from .auth import *
+    from .mail import *
+    from .api import *
+    from .restaurants import *
+    from .customers import *
+    from .inventory import *
+    from .orders import *
+    from .djext import *
+    from .reload import *
+    from .compressor import *
+except Exception as e:
+    print(f"error loading. .. {__file__}")
+    print(e)
+
---
+++ config/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for PLSOM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from config.env import env
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{env}")
+
+application = get_wsgi_application()
