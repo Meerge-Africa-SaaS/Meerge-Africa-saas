e42513a339e1552b620ab6dac46e6d2d68c2abe1
omniotosho <toloruntotosho@gmail.com>
2024-10-28 08:41:47 +0100

    settings re re

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,13 +1,11 @@
-# ruff: noqa: F405,F403,E402
-from config.environ import getenv
-#from mail import EMAIL_BACKEND, EMAIL_PORT, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_USE_TLS
+import os
 from .base import *  # noqa: F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
